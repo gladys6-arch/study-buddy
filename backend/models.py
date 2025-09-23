bd7e3048d7162ca2bd736769286b20d891a255df
gladys <achandogladys6@gmail.com>
2025-09-24 00:46:36 +0300

    relationships

--- backend/models.py
+++ backend/models.py
@@ -9,12 +9,16 @@ class Student(db.Model, SerializerMixin):
   id = db.Column(db.Integer, primary_key=True)
   name= db.Column(db.String, nullable=False)
 
+  study_sessions=db.relationship("StudySession", back_populates="students")
+
 
 class Subject(db.Model, SerializerMixin):
   __tablename__ = "subjects"
 
   id = db.Column(db.Integer, primary_key=True)
   name =db.Column(db.String, nullable=False)
+
+  tutors=db.relationship("Tutor", back_populates="subjects")
 
 
 class Tutor(db.Model, SerializerMixin):
@@ -23,6 +27,8 @@ class Tutor(db.Model, SerializerMixin):
   id= db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String, nullable=False)
 
+  subjects = db.relationship("Subject", back_populates="tutors")
+
 class StudySession(db.Model, SerializerMixin):
   __tablename__ = "study_sessions"
 
@@ -30,4 +36,11 @@ class StudySession(db.Model, SerializerMixin):
   notes = db.column(db.String)
   duration_minutes = db.Column(db.Integer)
 
-  
+  student_id = db.Column(db.Integer, db.ForeignKey("students.id"))
+  subject_id = db.Column(db.Integer, db.ForeignKey("subject.id"))
+
+# the relationships
+
+student = db.relationship("Student", back_populates="study_sessions")
+subject = db.relationship("Subject", back_populates="study_sessions")
+
