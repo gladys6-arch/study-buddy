7bde4b98c94d4a0ee694c87a5d91a6b139c1aa53
gladys <achandogladys6@gmail.com>
2025-09-25 11:16:20 +0300

    add new student

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <h1>Welcome</h1>;
+  return 
+  <div>
+    <h1 className="text-3xl font-bold mb-4">Welcome to Study Manager</h1>
+      <p className="text-gray-700">
+        Use the navigation bar to manage students, tutors, subjects, and study sessions.
+      </p>
+    
+  </div>
+
 }
