6d8a93c40b9f8837779c2050eb78b306e233be34
gladys <achandogladys6@gmail.com>
2025-09-25 11:01:43 +0300

    app.jsx correction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Students from "./pages/Students";
+import Subjects from "./pages/Subjects";
+import Tutors from "./pages/Tutors";
+import StudySession from "./pages/StudySession";
+
+
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1 p-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/students" element={<Students />} />
+          <Route path="/subjects" element={<Subjects />} />
+          <Route path="/tutors" element={<Tutors />} />
+          <Route path="/sessions" element={<StudySessions />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
 
